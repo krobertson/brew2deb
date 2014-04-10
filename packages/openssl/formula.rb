72cf5750a4c522486fcbbc08cf7fc5dd20bf1bd3
Ken Robertson <ken@apcera.com>
2014-04-10 10:50:44 -0700

    Update OpenSSL package for Ubuntu 13.04
    Includes patches pulled from Ubuntu's saucy update with fixes for CVE-2014-0076
    and CVE-2014-0160 (heartbleed).

--- packages/openssl/formula.rb
+++ packages/openssl/formula.rb
@@ -1,4 +1,16 @@
 class OpenSSL < DebianSourceFormula
-  url 'http://www.jp.backports.org/pool/main/o/openssl/openssl_0.9.8o-4~bpo50+1.dsc'
-  md5 '0ce765777672340889a9cad03ee50fe6'
+  url 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.0.1c-4ubuntu8.2.dsc'
+  md5 '4ef383f9233622981412a9c4c8818ae5'
+  version '1.0.1c-4ubuntu8.2apcera1'
+
+  def build
+    ENV['EDITOR'] = '/bin/true'
+    safe_system 'dpkg-source', '--commit', '.', 'brew2deb'
+    super
+  end
+
+  def patches
+    # packages retrieved from Ubuntu's openssl build for saucy on 2014-04-09
+    [ 'CVE-2014-0076.patch', 'CVE-2014-0160.patch' ]
+  end
 end
